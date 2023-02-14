fc5964c60520b1ec5d0e5a786cb1fe93964d2453
deepak choudhary <deepak32145@gmail.com>
2023-02-14 14:26:06 +0530

    initial commit

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Paper, IconButton } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+
+const SearchBar = () => {
+  return <div>SearchBar</div>;
+};
+
+export default SearchBar;
